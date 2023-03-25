4647ac947a51189baaa892502b929248d87a0c74
petesouth <pete_south@yahoo.com>
2023-03-25 03:07:33 +0000

    Hello world phaser typescript in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {AUTO, Game, Types} from "phaser";
+
+
+class TheScene extends Phaser.Scene {
+  create() {
+    this.add.text(100, 100, 'Hello there', { color: '#0f0' });
+  }
+}
+
+const TheGame = () => {
+
+  useEffect(() => {
+    const config: Types.Core.GameConfig = {
+      type: AUTO,
+      width: 800,
+      height: 600,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 200 }
+        }
+      },
+      scene: TheScene
+
+    };
+
+    const game = new Game(config);
+
+    return () => {
+      game.destroy(true);
+    }
+  }, []);
+
+  
+  return (
+    <div id="game-container"></div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TheGame />
     </div>
   );
 }
