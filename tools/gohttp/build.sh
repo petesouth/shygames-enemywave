4c0d965c3744c131bfa4180af266472e118fd070
petesouth <pete_south@yahoo.com>
2023-04-16 20:45:11 -0700

    Added paramters  addr  port  directory  websubpath  width  height

--- tools/gohttp/build.sh
+++ tools/gohttp/build.sh
@@ -6,13 +6,29 @@
 #The purpose of this is to show
 #the game in a window so it runs
 #as a local application/game (downloadable game)
-go get github.com/zserge/webview
-go install github.com/zserge/webview
+
+
+# ****** Make a windows Distributable
+GOARCH=windows go get github.com/zserge/webview
+GOARCH=windows go install github.com/zserge/webview
 
 #in the same way go get does
-go install ./gohttp.go
+GOARCH=windows go install ./gohttp.go
 
-cp $GOPATH/bin/gohttp ./dist
+cp $GOPATH/bin/gohttp ./dist/gohttpwindows
 
-echo "build was successful"
+
+
+# ******* Make a Linux Distributable
+GOARCH=linux go get github.com/zserge/webview
+GOARCH=linux go install github.com/zserge/webview
+
+#in the same way go get does
+GOARCH=linux go install ./gohttp.go
+
+cp $GOPATH/bin/gohttp ./dist/gohttplinux
+
+
+echo "build was successful pleas check ./dist for your generated files"
+ls -all ./dist
 
--- tools/gohttp/gohttp.go
+++ tools/gohttp/gohttp.go
@@ -4,6 +4,7 @@ import (
     "flag"
     "log"
     "time"
+    "strconv"
     "net/http"
     "github.com/zserge/webview"
 )
@@ -26,13 +27,13 @@ func startServer( addr:string, port: string, directory: string) {
 }
 
 
-func runAppWindow(websubpath:string) {
+func runAppWindow(websubpath:string, width: int, height: int) {
     w := webview.New(true)
 	defer w.Destroy()
 
 	// Set the window title and size
 	w.SetTitle("My Webview Window")
-	w.SetSize(800, 600, webview.HintNone)
+	w.SetSize(width, height, webview.HintNone)
 
 	// Load a webpage
 	w.Navigate(websubpath);
@@ -47,11 +48,25 @@ func main() {
     port := flag.String("port", "8000", "Port to listen on")
     directory := flag.String("directory", ".", "Root directory")
     websubpath := flag.String("websubpath", ".", "Sub path to the application start page")
+    widthstr := flag.String("width", ".", "Window width")
+    heightstr := flag.String("height", ".", "Window height")
     
+    width, err := strconv.ParseInt(widthstr, 10, 64)
+    if err != nil {
+        log.Fatal("ParseInt width: ", err)
+        return 
+    }
+
+    height, err := strconv.ParseInt(heightstr, 10, 64)
+    if err != nil {
+        log.Fatal("ParseInt height: ", err)
+        return 
+    }
+
 
     go startServer( addr, port, directory)
     time.Sleep(5000);
 
-    runAppWindow(websubpath);
+    runAppWindow(websubpath, width, height);
    
 }
