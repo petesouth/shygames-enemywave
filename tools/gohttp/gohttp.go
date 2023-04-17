6530de7cd9aaf75d1bba26460d49e7bf85db86e6
petesouth <pete_south@yahoo.com>
2023-04-16 20:26:57 -0700

    Functions added window control in

--- tools/gohttp/gohttp.go
+++ tools/gohttp/gohttp.go
@@ -3,15 +3,12 @@ package main
 import (
     "flag"
     "log"
+    "time"
     "net/http"
     "github.com/zserge/webview"
 )
 
-func main() {
-    // Define command line flags for address and port
-    addr := flag.String("addr", "", "Address to listen on")
-    port := flag.String("port", "8000", "Port to listen on")
-    directory := flag.String("directory", ".", "Root directory")
+func startServer( addr:string, port: string, directory: string) {
     flag.Parse()
 
     // Serve files from current directory
@@ -26,4 +23,35 @@ func main() {
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
+}
+
+
+func runAppWindow(websubpath:string) {
+    w := webview.New(true)
+	defer w.Destroy()
+
+	// Set the window title and size
+	w.SetTitle("My Webview Window")
+	w.SetSize(800, 600, webview.HintNone)
+
+	// Load a webpage
+	w.Navigate(websubpath);
+
+	// Run the event loop and display the window
+	w.Run()
+}
+
+func main() {
+    // Define command line flags for address and port
+    addr := flag.String("addr", "", "Address to listen on")
+    port := flag.String("port", "8000", "Port to listen on")
+    directory := flag.String("directory", ".", "Root directory")
+    websubpath := flag.String("websubpath", ".", "Sub path to the application start page")
+    
+
+    go startServer( addr, port, directory)
+    time.Sleep(5000);
+
+    runAppWindow(websubpath);
+   
 }
