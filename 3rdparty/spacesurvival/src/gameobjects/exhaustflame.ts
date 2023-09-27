8913790e1d10708513e40b78316dc335c5f66d81
petesouth <pete_south@yahoo.com>
2023-09-27 15:01:59 +0000

    Now I have a sputtering jet fuel object to animate the ship when it thrusts forward.  Also maed a little alinged on cntroid inner trianble to hlep it look more interesting

---
+++ 3rdparty/spacesurvival/src/gameobjects/exhaustflame.ts
@@ -0,0 +1,71 @@
+// gameobjects/exhaustflame.ts
+import Phaser from 'phaser';
+
+class FlamePoint extends Phaser.Geom.Point {
+    visible: boolean = true;
+}
+
+export class ExhaustFlame {
+    private points: FlamePoint[];
+    private graphics: Phaser.GameObjects.Graphics;
+    private visible: boolean = false;
+    private base: Phaser.Geom.Triangle;
+
+    constructor(private scene: Phaser.Scene, base: Phaser.Geom.Triangle) {
+        this.base = base;
+        this.graphics = scene.add.graphics({ lineStyle: { width: 2, color: 0xffcc00 } });
+        this.points = [];
+        for (let i = 0; i < 5; i++) {
+            this.points.push(new FlamePoint());
+        }
+    }
+
+    show() {
+        this.visible = true;
+    }
+
+    hide() {
+        this.visible = false;
+    }
+
+    update() {
+        const centroid = Phaser.Geom.Triangle.Centroid(this.base);
+        const angle = Math.atan2(this.base.y1 - centroid.y, this.base.x1 - centroid.x) + Math.PI;
+        const distance = Phaser.Math.Distance.Between(this.base.x2, this.base.y2, this.base.x3, this.base.y3) / 2;
+    
+        const activeDots = Phaser.Math.Between(1, 3); // Randomly choose between 1 and 4 dots for sputtering effect
+    
+        for (let i = 0; i < this.points.length; i++) {
+            const point = this.points[i];
+    
+            if (i < activeDots) {
+                const offset = (i - (this.points.length / 2)) * 10 + 34; 
+                point.x = centroid.x + (distance + offset) * Math.cos(angle);
+                point.y = centroid.y + (distance + offset * 2.5) * Math.sin(angle); 
+            } else {
+                point.x = -10; // Move the inactive dots off screen
+                point.y = -10; 
+            }
+        }
+    }
+    
+
+    render() {
+        this.graphics.clear();
+
+        if (!this.visible) {
+            return;
+        }
+
+        this.points.forEach((point) => {
+            const colors = [0xffa500, 0xff4500, 0xffd700, 0xff8c00]; // Colors representing fire: orange, red-orange, gold, dark orange
+            const chosenColor = Phaser.Utils.Array.GetRandom(colors); // Pick a random color from the list
+    
+            this.graphics.fillStyle(chosenColor);
+           
+            if (point.visible) {
+                this.graphics.fillPointShape(point, 8);
+            }
+        });
+    }
+}
--- 3rdparty/spacesurvival/src/gameobjects/playerspaceship.ts
+++ 3rdparty/spacesurvival/src/gameobjects/playerspaceship.ts
@@ -1,13 +1,11 @@
 // gameobjects/playerspaceship.ts
 import Phaser from 'phaser';
-import store from '../store/store';
-import { spaceshipActions } from '../store/spaceshipstore';
 import { ExhaustFlame } from './exhaustflame';
-
 
 export class PlayerSpaceship {
 
   private spaceShipShape: Phaser.Geom.Triangle;
+  private innerSpaceShipShape: Phaser.Geom.Triangle;
   private graphics: Phaser.GameObjects.Graphics;
   private rotationRate: number = 0.2;
   private thrust: number = 0.5;
@@ -21,7 +19,7 @@ export class PlayerSpaceship {
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
-    this.graphics = scene.add.graphics({ lineStyle: { width: 2, color: 0xff0000 } });
+    this.graphics = scene.add.graphics({ lineStyle: { width: 2, color: 0x808080 }, fillStyle: { color: 0xC0C0C0 } }); // Dark gray border
     const halfBaseWidth = 20;
     const halfHeight = 30;
 
@@ -31,48 +29,58 @@ export class PlayerSpaceship {
       400 + halfBaseWidth, 300 + halfHeight
     );
 
+    // Adjusted position for the inner triangle to move it slightly towards the bottom of the parent triangle
+    this.innerSpaceShipShape = new Phaser.Geom.Triangle(
+      400, 300 - halfHeight * 0.6,
+      400 - halfBaseWidth * 0.7, 300 + halfHeight * 0.75,
+      400 + halfBaseWidth * 0.7, 300 + halfHeight * 0.75
+    );
+
     this.leftKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     this.rightKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     this.upKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-    this.exhaustFlame = new ExhaustFlame(scene);
+    this.exhaustFlame = new ExhaustFlame(scene, this.spaceShipShape);
   }
 
   updateSpaceshipState() {
     this.graphics.clear();
+
+    const centroid = Phaser.Geom.Triangle.Centroid(this.spaceShipShape);
+
     if (this.leftKey?.isDown) {
-        Phaser.Geom.Triangle.Rotate(this.spaceShipShape, -this.rotationRate);
+      Phaser.Geom.Triangle.RotateAroundPoint(this.spaceShipShape, centroid, -this.rotationRate);
+      Phaser.Geom.Triangle.RotateAroundPoint(this.innerSpaceShipShape, centroid, -this.rotationRate);
     } else if (this.rightKey?.isDown) {
-        Phaser.Geom.Triangle.Rotate(this.spaceShipShape, this.rotationRate);
+      Phaser.Geom.Triangle.RotateAroundPoint(this.spaceShipShape, centroid, this.rotationRate);
+      Phaser.Geom.Triangle.RotateAroundPoint(this.innerSpaceShipShape, centroid, this.rotationRate);
     }
-    
+
     if (this.upKey?.isDown) {
-        const centroid = Phaser.Geom.Triangle.Centroid(this.spaceShipShape);
-        const deltaX = this.spaceShipShape.x1 - centroid.x;
-        const deltaY = this.spaceShipShape.y1 - centroid.y;
-        const angle = Math.atan2(deltaY, deltaX);
-        this.velocity.x += this.thrust * Math.cos(angle);
-        this.velocity.y += this.thrust * Math.sin(angle);
+      const deltaX = this.spaceShipShape.x1 - centroid.x;
+      const deltaY = this.spaceShipShape.y1 - centroid.y;
+      const angle = Math.atan2(deltaY, deltaX);
+      this.velocity.x += this.thrust * Math.cos(angle);
+      this.velocity.y += this.thrust * Math.sin(angle);
     }
+
     this.velocity.x *= this.damping;
     this.velocity.y *= this.damping;
-
     Phaser.Geom.Triangle.Offset(this.spaceShipShape, this.velocity.x, this.velocity.y);
-    const maxX = Math.max(this.spaceShipShape.x1, this.spaceShipShape.x2, this.spaceShipShape.x3);
-    const minX = Math.min(this.spaceShipShape.x1, this.spaceShipShape.x2, this.spaceShipShape.x3);
-    const maxY = Math.max(this.spaceShipShape.y1, this.spaceShipShape.y2, this.spaceShipShape.y3);
-    const minY = Math.min(this.spaceShipShape.y1, this.spaceShipShape.y2, this.spaceShipShape.y3);
-    
-    if (maxX < 0) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, this.scene.scale.width, 0);
-    } else if (minX > this.scene.scale.width) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, -this.scene.scale.width, 0);
+    Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, this.velocity.x, this.velocity.y);
+
+    this.graphics.fillTriangleShape(this.spaceShipShape);
+    this.graphics.fillTriangleShape(this.innerSpaceShipShape);
+
+    this.graphics.strokeTriangleShape(this.spaceShipShape);
+    this.graphics.strokeTriangleShape(this.innerSpaceShipShape);
+
+    if (this.upKey?.isDown) {
+      this.exhaustFlame.show();
+      this.exhaustFlame.update();
+    } else {
+      this.exhaustFlame.hide();
     }
 
-    if (maxY < 0) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, 0, this.scene.scale.height);
-    } else if (minY > this.scene.scale.height) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, 0, -this.scene.scale.height);
-    }
-    this.graphics.strokeTriangleShape(this.spaceShipShape);
+    this.exhaustFlame.render();
   }
 }
