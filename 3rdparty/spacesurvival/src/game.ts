f68447cc336d5a88ede15861f48e3ee4fdcafc6b
petesouth <pete_south@yahoo.com>
2023-09-28 02:41:20 +0000

    Got collide bounce code working between spaceship and spaceobject

--- 3rdparty/spacesurvival/src/game.ts
+++ 3rdparty/spacesurvival/src/game.ts
@@ -1,45 +1,57 @@
-// game.ts
 import Phaser from 'phaser';
 import { PlayerSpaceship } from './gameobjects/playerspaceship';
-
-
+import { SpaceObject } from './gameobjects/spaceobject';
 
 export class MainScene extends Phaser.Scene {
-  private spaceship!: PlayerSpaceship;
+    private spaceship!: PlayerSpaceship;
+    private spaceObjects: SpaceObject[] = [];
 
-  constructor() {
-    super('MainScene');
-  }
+    constructor() {
+        super('MainScene');
+    }
 
-  create() {
-    // Create an instance of the CustomSpaceship class
-    this.spaceship = new PlayerSpaceship(this);
-  }
+    create() {
+        this.spaceship = new PlayerSpaceship(this);
 
-  update() {
-    this.spaceship.updateSpaceshipState();
-  }
+        // Create a random number of space objects
+        const numObjects = Phaser.Math.Between(5, 10);
+        for (let i = 0; i < numObjects; i++) {
+            const spaceObj = new SpaceObject(this);
+            this.spaceObjects.push(spaceObj);
+        }
+    }
+
+    update() {
+        
+      this.spaceship.updateSpaceshipState(this.spaceObjects);
+
+        this.spaceObjects.forEach(spaceObj => {
+            spaceObj.update(this.spaceObjects);
+        });
+    }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  parent: 'game',
-  scene: [MainScene],
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: 'game',
+    scene: [MainScene],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
 };
 
 export default class Game extends Phaser.Game {
-  constructor() {
-    super(config);
-    window.addEventListener("resize", () => {
-      const w = window.innerWidth;
-      const h = window.innerHeight;
-      this.scale?.setGameSize(w, h);
-    });
-  }
+    constructor() {
+        super(config);
+
+        // Adjust game size on window resize
+        window.addEventListener("resize", () => {
+            const w = window.innerWidth;
+            const h = window.innerHeight;
+            this.scale.setGameSize(w, h);
+        });
+    }
 }
--- 3rdparty/spacesurvival/src/gameobjects/playerspaceship.ts
+++ 3rdparty/spacesurvival/src/gameobjects/playerspaceship.ts
@@ -1,108 +1,153 @@
-// gameobjects/playerspaceship.ts
 import Phaser from 'phaser';
 import { ExhaustFlame } from './exhaustflame';
+import { SpaceObject } from './spaceobject';
 
 export class PlayerSpaceship {
 
-  private spaceShipShape: Phaser.Geom.Triangle;
-  private innerSpaceShipShape: Phaser.Geom.Triangle;
-  private graphics: Phaser.GameObjects.Graphics;
-  private rotationRate: number = 0.2;
-  private thrust: number = 0.5;
-  private damping: number = 0.98;
-  private velocity: Phaser.Math.Vector2 = new Phaser.Math.Vector2(0, 0);
-  private leftKey?: Phaser.Input.Keyboard.Key;
-  private rightKey?: Phaser.Input.Keyboard.Key;
-  private upKey?: Phaser.Input.Keyboard.Key;
-  private scene: Phaser.Scene;
-  private exhaustFlame: ExhaustFlame;
+    private spaceShipShape: Phaser.Geom.Triangle;
+    private innerSpaceShipShape: Phaser.Geom.Triangle;
+    private graphics: Phaser.GameObjects.Graphics;
+    private rotationRate: number = 0.2;
+    private thrust: number = 0.5;
+    private damping: number = 0.98;
+    private velocity: Phaser.Math.Vector2 = new Phaser.Math.Vector2(0, 0);
+    private leftKey?: Phaser.Input.Keyboard.Key;
+    private rightKey?: Phaser.Input.Keyboard.Key;
+    private upKey?: Phaser.Input.Keyboard.Key;
+    private scene: Phaser.Scene;
+    private exhaustFlame: ExhaustFlame;
 
-  constructor(scene: Phaser.Scene) {
-    this.scene = scene;
-    this.graphics = scene.add.graphics({ lineStyle: { width: 2, color: 0x808080 }, fillStyle: { color: 0xC0C0C0 } }); // Dark gray border
-    const halfBaseWidth = 20;
-    const halfHeight = 30;
+    constructor(scene: Phaser.Scene) {
+        this.scene = scene;
+        this.graphics = scene.add.graphics({ lineStyle: { width: 2, color: 0x808080 }, fillStyle: { color: 0xC0C0C0 } });
 
-    this.spaceShipShape = new Phaser.Geom.Triangle(
-      400, 300 - halfHeight,
-      400 - halfBaseWidth, 300 + halfHeight,
-      400 + halfBaseWidth, 300 + halfHeight
-    );
+        const halfBaseWidth = 20;
+        const halfHeight = 30;
 
-    // Adjusted position for the inner triangle to move it slightly towards the bottom of the parent triangle
-    this.innerSpaceShipShape = new Phaser.Geom.Triangle(
-      400, 300 - halfHeight * 0.6,
-      400 - halfBaseWidth * 0.7, 300 + halfHeight * 0.75,
-      400 + halfBaseWidth * 0.7, 300 + halfHeight * 0.75
-    );
+        this.spaceShipShape = new Phaser.Geom.Triangle(
+            400, 300 - halfHeight,
+            400 - halfBaseWidth, 300 + halfHeight,
+            400 + halfBaseWidth, 300 + halfHeight
+        );
 
-    this.leftKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    this.rightKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    this.upKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-    this.exhaustFlame = new ExhaustFlame(scene, this.spaceShipShape);
-  }
+        this.innerSpaceShipShape = new Phaser.Geom.Triangle(
+            400, 300 - halfHeight * 0.6,
+            400 - halfBaseWidth * 0.7, 300 + halfHeight * 0.75,
+            400 + halfBaseWidth * 0.7, 300 + halfHeight * 0.75
+        );
 
-  updateSpaceshipState() {
-    this.graphics.clear();
+        this.leftKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.rightKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.upKey = scene.input?.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
 
-    const centroid = Phaser.Geom.Triangle.Centroid(this.spaceShipShape);
-
-    if (this.leftKey?.isDown) {
-        Phaser.Geom.Triangle.RotateAroundPoint(this.spaceShipShape, centroid, -this.rotationRate);
-        Phaser.Geom.Triangle.RotateAroundPoint(this.innerSpaceShipShape, centroid, -this.rotationRate);
-    } else if (this.rightKey?.isDown) {
-        Phaser.Geom.Triangle.RotateAroundPoint(this.spaceShipShape, centroid, this.rotationRate);
-        Phaser.Geom.Triangle.RotateAroundPoint(this.innerSpaceShipShape, centroid, this.rotationRate);
+        this.exhaustFlame = new ExhaustFlame(scene, this.spaceShipShape);
     }
 
-    if (this.upKey?.isDown) {
-        const deltaX = this.spaceShipShape.x1 - centroid.x;
-        const deltaY = this.spaceShipShape.y1 - centroid.y;
-        const angle = Math.atan2(deltaY, deltaX);
-        this.velocity.x += this.thrust * Math.cos(angle);
-        this.velocity.y += this.thrust * Math.sin(angle);
+    updateSpaceshipState(spaceObjects: SpaceObject[]) {
+        this.graphics.clear();
+
+        const centroid = Phaser.Geom.Triangle.Centroid(this.spaceShipShape);
+
+        if (this.leftKey?.isDown) {
+            Phaser.Geom.Triangle.RotateAroundPoint(this.spaceShipShape, centroid, -this.rotationRate);
+            Phaser.Geom.Triangle.RotateAroundPoint(this.innerSpaceShipShape, centroid, -this.rotationRate);
+        } else if (this.rightKey?.isDown) {
+            Phaser.Geom.Triangle.RotateAroundPoint(this.spaceShipShape, centroid, this.rotationRate);
+            Phaser.Geom.Triangle.RotateAroundPoint(this.innerSpaceShipShape, centroid, this.rotationRate);
+        }
+
+        if (this.upKey?.isDown) {
+            const deltaX = this.spaceShipShape.x1 - centroid.x;
+            const deltaY = this.spaceShipShape.y1 - centroid.y;
+            const angle = Math.atan2(deltaY, deltaX);
+            this.velocity.x += this.thrust * Math.cos(angle);
+            this.velocity.y += this.thrust * Math.sin(angle);
+        }
+
+        this.velocity.x *= this.damping;
+        this.velocity.y *= this.damping;
+
+        Phaser.Geom.Triangle.Offset(this.spaceShipShape, this.velocity.x, this.velocity.y);
+        Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, this.velocity.x, this.velocity.y);
+
+        const maxX = Math.max(this.spaceShipShape.x1, this.spaceShipShape.x2, this.spaceShipShape.x3);
+        const minX = Math.min(this.spaceShipShape.x1, this.spaceShipShape.x2, this.spaceShipShape.x3);
+        const maxY = Math.max(this.spaceShipShape.y1, this.spaceShipShape.y2, this.spaceShipShape.y3);
+        const minY = Math.min(this.spaceShipShape.y1, this.spaceShipShape.y2, this.spaceShipShape.y3);
+
+        if (maxX < 0) {
+            Phaser.Geom.Triangle.Offset(this.spaceShipShape, this.scene.scale.width, 0);
+            Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, this.scene.scale.width, 0);
+        } else if (minX > this.scene.scale.width) {
+            Phaser.Geom.Triangle.Offset(this.spaceShipShape, -this.scene.scale.width, 0);
+            Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, -this.scene.scale.width, 0);
+        }
+
+        if (maxY < 0) {
+            Phaser.Geom.Triangle.Offset(this.spaceShipShape, 0, this.scene.scale.height);
+            Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, 0, this.scene.scale.height);
+        } else if (minY > this.scene.scale.height) {
+            Phaser.Geom.Triangle.Offset(this.spaceShipShape, 0, -this.scene.scale.height);
+            Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, 0, -this.scene.scale.height);
+        }
+
+
+        for (const spaceObj of spaceObjects) {
+            const distance = Phaser.Math.Distance.Between(this.spaceShipShape.x1, this.spaceShipShape.y1, 
+                spaceObj.getPolygon().points[0].x, spaceObj.getPolygon().points[0].y);
+            if (distance < 40) {
+                // Calculate collision angle
+                const angle = Phaser.Math.Angle.Between(
+                    this.spaceShipShape.x1,
+                    this.spaceShipShape.y1,
+                    spaceObj.getPolygon().points[0].x,
+                    spaceObj.getPolygon().points[0].y
+                );        
+                // Create Vector2 objects for velocity and position
+                const velocity1 = new Phaser.Math.Vector2(this.velocity.x, this.velocity.y);
+                const velocity2 = spaceObj.getVelocity().clone();
+                const position1 = new Phaser.Math.Vector2(this.spaceShipShape.x1, this.spaceShipShape.y1);
+                const position2 = { ...spaceObj.getPolygon().points[0] };
+                
+                // Calculate new velocities for both objects based on collision
+                const m1 = 1; // Mass of PlayerSpaceship (adjust as needed)
+                const m2 = 1; // Mass of SpaceObject (adjust as needed)
+                
+                const newVelocity1 = velocity1
+                    .clone()
+                    .scale((m1 - m2) / (m1 + m2))
+                    .add(
+                        velocity2
+                            .clone()
+                            .scale((2 * m2) / (m1 + m2))
+                    );
+                
+                const newVelocity2 = velocity2
+                    .clone()
+                    .scale((m2 - m1) / (m1 + m2))
+                    .add(
+                        velocity1
+                            .clone()
+                            .scale((2 * m1) / (m1 + m2))
+                    );
+        
+                // Apply new velocities in opposite directions
+                this.velocity.set(newVelocity1.x, newVelocity1.y);
+                spaceObj.setVelocity(newVelocity2.x, newVelocity2.y);
+            }
+        }
+                
+
+        this.graphics.strokeTriangleShape(this.spaceShipShape);
+        this.graphics.fillTriangleShape(this.innerSpaceShipShape);
+
+        if (this.upKey?.isDown) {
+            this.exhaustFlame.show();
+            this.exhaustFlame.update();
+        } else {
+            this.exhaustFlame.hide();
+        }
+
+        this.exhaustFlame.render();
     }
-
-    this.velocity.x *= this.damping;
-    this.velocity.y *= this.damping;
-    Phaser.Geom.Triangle.Offset(this.spaceShipShape, this.velocity.x, this.velocity.y);
-    Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, this.velocity.x, this.velocity.y);
-
-    const maxX = Math.max(this.spaceShipShape.x1, this.spaceShipShape.x2, this.spaceShipShape.x3);
-    const minX = Math.min(this.spaceShipShape.x1, this.spaceShipShape.x2, this.spaceShipShape.x3);
-    const maxY = Math.max(this.spaceShipShape.y1, this.spaceShipShape.y2, this.spaceShipShape.y3);
-    const minY = Math.min(this.spaceShipShape.y1, this.spaceShipShape.y2, this.spaceShipShape.y3);
-
-    if (maxX < 0) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, this.scene.scale.width, 0);
-        Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, this.scene.scale.width, 0);
-    } else if (minX > this.scene.scale.width) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, -this.scene.scale.width, 0);
-        Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, -this.scene.scale.width, 0);
-    }
-
-    if (maxY < 0) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, 0, this.scene.scale.height);
-        Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, 0, this.scene.scale.height);
-    } else if (minY > this.scene.scale.height) {
-        Phaser.Geom.Triangle.Offset(this.spaceShipShape, 0, -this.scene.scale.height);
-        Phaser.Geom.Triangle.Offset(this.innerSpaceShipShape, 0, -this.scene.scale.height);
-    }
-
-    this.graphics.fillTriangleShape(this.spaceShipShape);
-    this.graphics.fillTriangleShape(this.innerSpaceShipShape);
-
-    this.graphics.strokeTriangleShape(this.spaceShipShape);
-    this.graphics.strokeTriangleShape(this.innerSpaceShipShape);
-
-    if (this.upKey?.isDown) {
-        this.exhaustFlame.show();
-        this.exhaustFlame.update();
-    } else {
-        this.exhaustFlame.hide();
-    }
-
-    this.exhaustFlame.render();
-}
-
 }
