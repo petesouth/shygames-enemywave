887d498b5b1116572149776bf47438ca4ec6e169
petesouth <pete_south@yahoo.com>
2023-09-28 04:00:53 +0000

    Improvements in the bouncy ness of rockes.

--- 3rdparty/spacesurvival/src/gameobjects/playerspaceship.ts
+++ 3rdparty/spacesurvival/src/gameobjects/playerspaceship.ts
@@ -120,7 +120,7 @@ export class PlayerSpaceship {
 
                 const distance = Phaser.Math.Distance.Between(x1, y1, spaceObj.getPolygon().points[0].x, spaceObj.getPolygon().points[0].y);
 
-                if (distance < 20) {
+                if (distance < 40) {
                     // Calculate collision angle
                     const angle = Phaser.Math.Angle.Between(x1, y1, spaceObj.getPolygon().points[0].x, spaceObj.getPolygon().points[0].y);
                     // Create Vector2 objects for velocity and position
--- 3rdparty/spacesurvival/src/gameobjects/spaceobject.ts
+++ 3rdparty/spacesurvival/src/gameobjects/spaceobject.ts
@@ -48,6 +48,14 @@ export class SpaceObject {
             return new Phaser.Geom.Point(point.x + this.velocity.x, point.y + this.velocity.y);
         }));
 
+        // Clamp the velocity magnitude to a maximum value
+        const maxVelocityMagnitude = 7; // Adjust as needed
+        const velocityMagnitude = this.velocity.length();
+
+        if (velocityMagnitude > maxVelocityMagnitude) {
+            this.velocity.normalize().scale(maxVelocityMagnitude);
+        }
+
         const maxX = Math.max(...this.polygon.points.map(point => point.x));
         const minX = Math.min(...this.polygon.points.map(point => point.x));
         const maxY = Math.max(...this.polygon.points.map(point => point.y));
@@ -66,7 +74,7 @@ export class SpaceObject {
         }
 
         this.graphics.clear();
-        
+
         this.graphics.fillStyle(0x777777);
 
         // Draw the polygon with both fill and stroke
@@ -77,34 +85,30 @@ export class SpaceObject {
         this.detectCollisions(spaceObjects);
     }
 
-    private detectCollisions(spaceObjects: SpaceObject[]) {
-        if (!this.hasCollided) { // Check if a collision has occurred in the current frame
-            for (const spaceObj of spaceObjects) {
-                if (spaceObj !== this) { // Avoid self-collision
-                    const collisionPoints = this.polygon.points;
 
+    private detectCollisions(spaceObjects: SpaceObject[]) {
+        if (!this.hasCollided) {
+            for (const spaceObj of spaceObjects) {
+                if (spaceObj !== this) {
+                    const collisionPoints = this.polygon.points;
+    
                     for (let i = 0; i < collisionPoints.length; i++) {
                         const x1 = collisionPoints[i].x;
                         const y1 = collisionPoints[i].y;
                         const x2 = collisionPoints[(i + 1) % collisionPoints.length].x;
                         const y2 = collisionPoints[(i + 1) % collisionPoints.length].y;
-
+    
                         const distance = Phaser.Math.Distance.Between(x1, y1, spaceObj.getPolygon().points[0].x, spaceObj.getPolygon().points[0].y);
-
-                        if (distance < 30) { // Check if the objects are close enough to collide
-                            // Calculate collision angle with some randomness
+    
+                        if (distance < 30) {
                             const angle = Phaser.Math.Angle.Between(x1, y1, spaceObj.getPolygon().points[0].x, spaceObj.getPolygon().points[0].y) + Phaser.Math.RND.realInRange(-Math.PI / 4, Math.PI / 4);
-
-                            // Create Vector2 objects for velocity and position
+    
                             const velocity1 = this.velocity.clone();
                             const velocity2 = spaceObj.getVelocity().clone();
-                            const position1 = new Phaser.Math.Vector2(x1, y1);
-                            const position2 = { ...spaceObj.getPolygon().points[0] };
-
-                            // Calculate new velocities for both objects based on collision
+    
                             const m1 = 1; // Mass of the first object (adjust as needed)
                             const m2 = 1; // Mass of the second object (adjust as needed)
-
+    
                             const newVelocity1 = velocity1
                                 .clone()
                                 .scale((m1 - m2) / (m1 + m2))
@@ -113,7 +117,7 @@ export class SpaceObject {
                                         .clone()
                                         .scale((2 * m2) / (m1 + m2))
                                 );
-
+    
                             const newVelocity2 = velocity2
                                 .clone()
                                 .scale((m2 - m1) / (m1 + m2))
@@ -122,28 +126,29 @@ export class SpaceObject {
                                         .clone()
                                         .scale((2 * m1) / (m1 + m2))
                                 );
-
-                            // Apply new velocities in opposite directions
+    
                             this.velocity.set(newVelocity1.x, newVelocity1.y);
                             spaceObj.setVelocity(newVelocity2.x, newVelocity2.y);
-
-                            // Set the collision flag to true to maintain the new speed until the next frame
+    
                             this.hasCollided = true;
                             spaceObj.hasCollided = true;
-
-                            // Add a separation force to avoid sticking
-                            const separationForce = new Phaser.Math.Vector2(Math.cos(angle) * 2, Math.sin(angle) * 2);
-                            this.velocity.add(separationForce);
-                            spaceObj.getVelocity().subtract(separationForce);
+    
+                            // Calculate the repulsion force to avoid sticking
+                            const repulsionForce = new Phaser.Math.Vector2(Math.cos(angle), Math.sin(angle));
+                            repulsionForce.scale(1 / distance); // Inverse proportion to distance
+    
+                            // Apply the repulsion force to push the objects apart
+                            this.velocity.add(repulsionForce);
+                            spaceObj.getVelocity().subtract(repulsionForce);
                         }
                     }
                 }
             }
         } else {
-            // Reset the collision flag to false if no collision occurred in the current frame
             this.hasCollided = false;
         }
     }
+    
 
     getPolygon() {
         return this.polygon;
