413943ab992d9930fdee587f632d8d6246947f0d
petesouth <pete_south@yahoo.com>
2023-10-28 07:38:21 +0000

    Latest fix for static site

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const AppRouter = () => {
 
   return (<Router >
     <Routes>
-      <Route path="/docs/" element={<AppMainGameFeed />} />
+      <Route path="/shyhumangames-gamefeed/" element={<AppMainGameFeed />} />
       <Route path="/" element={<AppMainGameFeed />} />
     </Routes>
   </Router>)
